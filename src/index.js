bc88d3891c085ebcc5773b5a4e9c026361c2b970
walter <buczacka.walter@gmail.com>
2018-05-19 11:56:18 -0300

    rename root index class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import mySaga from './sagas';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+import reducers from './reducers';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducers, {}, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(mySaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
